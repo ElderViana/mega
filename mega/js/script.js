e3fcb895819f0bb2b4ae783ecdf4fc6ad726d88b
Elder Viana <elder.viana@fabrica.locaweb.com.br>
2024-12-03 20:41:38 -0300

    e

---
+++ mega/js/script.js
@@ -0,0 +1,11 @@
+const greeting = (name) => alert('Hello' + name);
+
+const processUserInput = (callback) => {
+    const name = prompt('type your name:');
+    callback(name);
+}
+
+
+processUserInput
+
+const number = 2;
